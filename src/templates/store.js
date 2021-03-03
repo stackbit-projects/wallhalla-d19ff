74be19275d0d3f55d48238b6412dd96c300fcb78
Stackbit Code Editor <support@stackbit.com>
2021-03-03 01:45:13 +0000

    store.js: changed

--- src/templates/store.js
+++ src/templates/store.js
@@ -19,6 +19,8 @@ export default class Store extends React.Component {
         return (
             <Layout {...this.props}>
             <main className={'content' + (_.get(this.props, 'pageContext.frontmatter.page_css_class', null) ? (' ' + _.get(this.props, 'pageContext.frontmatter.page_css_class', null)) : '')}>
+                
+                
                 {_.map(_.get(this.props, 'pageContext.frontmatter.sections', null), (section, section_idx) => {
                     let component = _.upperFirst(_.camelCase(_.get(section, 'type', null)));
                     let Component = components[component];
