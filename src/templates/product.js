8a48ca94417ff96d063fc529a56748b6f937af61
Stackbit Code Editor <projects@stackbit.com>
2021-03-25 19:44:08 +0000

    product.js: changed

--- src/templates/product.js
+++ src/templates/product.js
@@ -25,6 +25,7 @@ export const query = graphql`
 
 
 export default class Product extends React.Component {
+    
     render() {
         let primary_bg_img_opacity_pct = _.get(this.props, 'pageContext.site.siteMetadata.bg_image_primary_opacity', null) || 100;
         let primary_bg_img_opacity = primary_bg_img_opacity_pct * 0;
