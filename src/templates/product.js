05751b16df064ff2eb30aae50b1824fe01366a33
Stackbit Code Editor <projects@stackbit.com>
2021-03-25 19:10:39 +0000

    product.js: changed

--- src/templates/product.js
+++ src/templates/product.js
@@ -69,38 +69,38 @@ export default class Product extends React.Component {
                             </article>
 
                             class MyForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      username: '',
-      age: null,
-    };
-  }
-  myChangeHandler = (event) => {
-    let nam = event.target.name;
-    let val = event.target.value;
-    this.setState({[nam]: val});
-  }
-  render() {
-    return (
-      <form>
-      <h1>Hello {this.state.username} {this.state.age}</h1>
-      <p>Enter your name:</p>
-      <input
-        type='text'
-        name='username'
-        onChange={this.myChangeHandler}
-      />
-      <p>Enter your age:</p>
-      <input
-        type='text'
-        name='age'
-        onChange={this.myChangeHandler}
-      />
-      </form>
-    );
-  }
-}    
+                                constructor(props) {
+                                    super(props);
+                                    this.state = {
+                                    username: '',
+                                    age: null,
+                                    };
+                                }
+                                myChangeHandler = (event) => {
+                                    let nam = event.target.name;
+                                    let val = event.target.value;
+                                    this.setState({[nam]: val});
+                                }
+                                render() {
+                                    return (
+                                    <form>
+                                    <h1>Hello {this.state.username} {this.state.age}</h1>
+                                    <p>Enter your name:</p>
+                                    <input
+                                        type='text'
+                                        name='username'
+                                        onChange={this.myChangeHandler}
+                                    />
+                                    <p>Enter your age:</p>
+                                    <input
+                                        type='text'
+                                        name='age'
+                                        onChange={this.myChangeHandler}
+                                    />
+                                    </form>
+                                    );
+                                }
+                                }    
 
 
 
