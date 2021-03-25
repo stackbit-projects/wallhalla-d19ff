efae9911f3fb80683056daea5f82cead6d35770b
Stackbit Code Editor <projects@stackbit.com>
2021-03-25 19:30:03 +0000

    product.js: changed

--- src/templates/product.js
+++ src/templates/product.js
@@ -80,8 +80,6 @@ export default class Product extends React.Component {
                                 {_.get(this.props, 'pageContext.frontmatter.description', null)}
                             </article>
 
-
-
                             <form>
                             <h1>Hello {this.state.username} {this.state.age}</h1>
                             <p>Enter your name:</p>
