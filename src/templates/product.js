abb9ae021af30820676082e8b14ec638d3fe3f52
Stackbit Code Editor <projects@stackbit.com>
2021-03-25 19:28:35 +0000

    product.js: changed

--- src/templates/product.js
+++ src/templates/product.js
@@ -68,19 +68,7 @@ export default class Product extends React.Component {
                                 {_.get(this.props, 'pageContext.frontmatter.description', null)}
                             </article>
 
-                            class MyForm extends React.Component {
-                                constructor(props) {
-                                    super(props);
-                                    this.state = {
-                                    username: '',
-                                    age: null,
-                                    };
-                                }
-                                myChangeHandler = (event) => {
-                                    let nam = event.target.name;
-                                    let val = event.target.value;
-                                    this.setState({[nam]: val});
-                                }
+
 
                                     <form>
                                     <h1>Hello {this.state.username} {this.state.age}</h1>
