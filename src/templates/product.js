9559a6766e7cddaeecee3c174daeec9be15aad12
Stackbit Code Editor <projects@stackbit.com>
2021-03-25 19:35:05 +0000

    product.js: changed

--- src/templates/product.js
+++ src/templates/product.js
@@ -68,9 +68,7 @@ export default class Product extends React.Component {
                             </article>
    
                             <form>
-                                <p>Enter your name:</p>  <input
-                                type="text"
-                                />
+                                <p>Enter your name:</p>  <input                       type="text"                             />
                             </form>
    
 
