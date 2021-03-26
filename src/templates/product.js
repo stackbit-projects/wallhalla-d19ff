d1c09cbb852ed5ead29d540b6c0d2f0f86fac11f
Stackbit Code Editor <projects@stackbit.com>
2021-03-26 15:07:40 +0000

    product.js: changed

--- src/templates/product.js
+++ src/templates/product.js
@@ -66,7 +66,7 @@ export default class Product extends React.Component {
                             <article className="product__description">
                                 {_.get(this.props, 'pageContext.frontmatter.description', null)}
                                 <form>
-                                    <p>.</p>
+                                    <p><br></br></p>
                                     <p>Widht of your wall in inches :  <input  type="text" /></p>
                                     <p>Height of your wall in inches:  <input  type="text" /></p>
                                     <p>Colour code background       :  <input  type="text" /></p>
