5612058180a8c76441e68fe215366ad7cf1f6396
Stackbit Code Editor <projects@stackbit.com>
2021-03-26 14:32:21 +0000

    product.js: changed

--- src/templates/product.js
+++ src/templates/product.js
@@ -68,7 +68,7 @@ export default class Product extends React.Component {
                                 <form>
                                     <p>.</p>
                                     <p>Widht of your wall in inches :  <input  type="text" /></p>
-                                    <p>height of your wall in inches:  <input  type="text" /></p>
+                                    <p>Height of your wall in inches:  <input  type="text" /></p>
                                     <p>Colour code background       :  <input  type="text" /></p>
                                     <p>Colour code text       :  <input  type="text" /></p>
                                     <p>.</p>
