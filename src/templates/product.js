354f0cc78b0d8b1eef069640d80638113c29094a
Stackbit Code Editor <projects@stackbit.com>
2021-03-26 15:07:15 +0000

    product.js: changed

--- src/templates/product.js
+++ src/templates/product.js
@@ -75,7 +75,7 @@ export default class Product extends React.Component {
                                 </form>
                             </article>
                             <img src='/images/colorexample.svg' />
-                            <p><br></p>
+                            <p><br><br></p>
                             
                             <BuyButton {...this.props} product_page={this.props.pageContext} />
                         </div>
