a639b42badf1cd56f2016463bf8320f64450bc68
Stackbit Code Editor <projects@stackbit.com>
2021-03-30 02:53:25 +0000

    gatsby-ssr.js: changed

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -19,6 +19,7 @@ exports.onRenderBody = function({ setHeadComponents, setPostBodyComponents }) {
             <script src={withPrefix('js/main.js')}/>
             <script src={withPrefix('js/page_load.js')}/>
             <script src={withPrefix('js/page_unload.js')}/>
+            <script src="https://cdn.jsdelivr.net/npm/@jaames/iro@5"></script>
             
         </React.Fragment>
     ]);
